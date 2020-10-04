05ba6664f54c1bc5e11f06f25927e5b1cea1e748
Jeroen Mies <jeroen.mies@rws.nl>
2020-10-04 19:53:21 +0200

    feat: Add Speakers Controller

--- src/main/java/com/pluralsight/conferencedemo/controllers/SessionsController.java
+++ src/main/java/com/pluralsight/conferencedemo/controllers/SessionsController.java
@@ -3,7 +3,6 @@ package com.pluralsight.conferencedemo.controllers;
 import com.pluralsight.conferencedemo.models.Session;
 import com.pluralsight.conferencedemo.repositories.SessionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
