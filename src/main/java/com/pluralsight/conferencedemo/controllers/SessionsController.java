08b12ec185cde5b22c5fc49c508ee519b8911a0d
Jeroen Mies <jeroen.mies@rws.nl>
2020-10-05 12:11:49 +0200

    feat: adding app version controller

--- src/main/java/com/pluralsight/conferencedemo/controllers/SessionsController.java
+++ src/main/java/com/pluralsight/conferencedemo/controllers/SessionsController.java
@@ -1,7 +1,6 @@
 package com.pluralsight.conferencedemo.controllers;
 
 import com.pluralsight.conferencedemo.models.Session;
-import com.pluralsight.conferencedemo.models.Speaker;
 import com.pluralsight.conferencedemo.repositories.SessionRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
